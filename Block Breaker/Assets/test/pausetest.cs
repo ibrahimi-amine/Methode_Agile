e8e1f612c161248787b9e74f82d156b80599c57c
hajaratbib <60238628+hajaratbib@users.noreply.github.com>
2020-05-29 12:04:18 +0000

    Add files via upload

---
+++ Block Breaker/Assets/test/pausetest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausetest
+{
+
+   public void ispausedreturnstrue(){
+    Input.GetKeyDown(KeyCode.Escape)=true;
+       //arrange
+       var PauseMenu = new PauseMenu();
+       //act
+       PauseMenu.update(isPaused=true);
+       //assert
+       Assert.AreEqual(isPaused,false);
+   }
+
+    public void ispausedreturnsfalse(){
+    Input.GetKeyDown(KeyCode.Escape)=true;
+       //arrange
+       var PauseMenu = new PauseMenu();
+       //act
+       PauseMenu.update(isPaused=false);
+       //assert
+       Assert.AreEqual(ispaused,true);
+      }
+
+}
