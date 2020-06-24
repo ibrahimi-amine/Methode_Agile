bb412b10863807bac4ede4437ed4a42624b7f595
hajaratbib <60238628+hajaratbib@users.noreply.github.com>
2020-06-24 18:48:46 +0000

    Add files via upload

---
+++ pausetest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework
+
+public class Pausetest
+{
+
+   [Test]
+   public void ispausedreturnstrue(){
+    Input.GetKeyDown(KeyCode.Escape)=true;
+       //arrange
+       var PauseMenu = new PauseMenu();
+       //act
+       PauseMenu.update(isPaused=true);
+       //assert
+       Assert.AreEqual(isPaused,false);
+   }
+
+
+    [Test]
+    public void ispausedreturnsfalse(){
+    Input.GetKeyDown(KeyCode.Escape)=true;
+       //arrange
+       var PauseMenu = new PauseMenu();
+       //act
+       PauseMenu.update(isPaused=false);
+       //assert
+       Assert.AreEqual(ispaused,true);
+      }
+
+}
